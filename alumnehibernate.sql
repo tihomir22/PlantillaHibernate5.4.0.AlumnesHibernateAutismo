144fc3c6630b965c0e72b67bd6b1f7b8c02ccb7b
tihomir22 <tihomir_alcudia3@hotmail.com>
2019-01-28 21:09:04 +0100

    añadido sql

--- alumnehibernate.sql
+++ alumnehibernate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-01-2019 a las 18:44:12
+-- Tiempo de generación: 28-01-2019 a las 21:08:15
 -- Versión del servidor: 10.1.29-MariaDB
 -- Versión de PHP: 7.2.0
 
